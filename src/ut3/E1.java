60e9f461067ea45366fb13db920d0ef551306c1c
Daasty <daastiiy@gmail.com>
2018-12-11 14:16:26 +0100

    E2 a E5

--- src/ut3/E1.java
+++ src/ut3/E1.java
@@ -1,4 +1,26 @@
 package ut3;
 
+import java.util.Scanner;
+
+/**
+ * Programa que pida la altura y el peso al usuario para mostrar el IMC.
+ * IMC -> IMC = peso/altura^2
+ * Indicar si el peso es ideal o no, que es entre 18 y 25.
+ */
 public class E1 {
+    public static void main (String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Dime tu altura en metros: ");
+        double altura = Math.pow(Double.parseDouble(teclado.nextLine()), 2);
+
+        System.out.println("Dime el peso en kg: ");
+        double peso = teclado.nextDouble();
+
+        double imc = peso / altura;
+
+        System.out.printf("Tu IMC es %f \n", imc);
+        if (imc > 18 && imc < 25) {
+            System.out.println("Tu IMC es adecuado.");
+        }
+    }
 }
