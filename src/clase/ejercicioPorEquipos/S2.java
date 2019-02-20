d204f4858aaabd41c8b7973cce9348eabd097cf6
Daasty <daastiiy@gmail.com>
2019-02-20 18:53:02 +0100

    S3 y S4

--- src/clase/ejercicioPorEquipos/S2.java
+++ src/clase/ejercicioPorEquipos/S2.java
@@ -1,12 +1,15 @@
 package clase.ejercicioPorEquipos;
 
 import java.text.Collator;
+import java.util.Scanner;
 
 /**
  * Listado de resultados de un determinado equipo que se debe pedir por teclado
  */
 public class S2 {
     public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+
         String cities[] = {"Leeds", "Madrid", "Bilbao", "Lisboa", "Oporto", "París"};
         String teams[] = {"Black door", "Blizzard", "D&D Robotics", "Renegades", "Steel tigers", "TK raptors"};
 
@@ -20,8 +23,25 @@ public class S2 {
         Collator comp = Collator.getInstance();
         comp.setStrength(Collator.PRIMARY);
 
+        System.out.println("Dime el equipo del que quieres saber los resultados: ");
 
+        String equipo =  teclado.nextLine().trim();
+        int posicion = -1;
 
+        for (int i = 0; i < teams.length; i++) {
+                if (comp.compare(teams[i], equipo) == 0) {
+                    posicion = i;
+                    break;
+            }
+        }
+
+        if (posicion < 0) {
+            System.out.println("No encontrado.");
+        }
+
+        for (int i = 0; i < cities.length; i++) {
+            System.out.printf("%-12s  :  %d\n", cities[posicion], pos[posicion][i]);
+        }
 
     }
 }
