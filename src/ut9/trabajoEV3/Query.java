81b47460d033c872b16c6e43500c7885fd965708
Daasty <daastiiy@gmail.com>
2019-06-17 22:07:58 +0200

    Trabajo EV3 v1.1

--- src/ut9/trabajoEV3/Query.java
+++ src/ut9/trabajoEV3/Query.java
@@ -1,6 +1,8 @@
 package ut9.trabajoEV3;
 
-import java.sql.*;
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.ResultSet;
 
 /**
  * @author daast
@@ -8,24 +10,24 @@ import java.sql.*;
 
 public class Query {
     public static void query() {
-        try (
-                Connection cn = Connect.connection();
-                CallableStatement cs = cn.prepareCall("{call precioMedio}");
-                ResultSet rs = cs.executeQuery()
-        ) {
+        try {
+            Connection cn = Connect.connection();
+            CallableStatement cs = cn.prepareCall("{call precioMedio}");
+
+            ResultSet rs = cs.executeQuery();
+
+
             System.out.println("[Consulta]");
             System.out.printf("%-18s", "Precio Medio: ");
             while (rs.next()) {
                 double precio = rs.getDouble("Precio Medio");
 
-                System.out.printf("%7.0f mods\n", precio);
+                System.out.printf("%3.0f euros\n", precio);
             }
-
 
         } catch (Exception e) {
             System.out.println("Error> " + e.getMessage());
         }
 
     }
-
 }
